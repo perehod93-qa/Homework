4cc9c29b8ba8dac11a27acafab3e6badcff7f4f7
perehod93-qa <perehod_93@inbox.ru>
2025-10-22 13:30:01 +0300

    lesson_4 done

--- src/main/java/lesson_3/park/Park.java
+++ src/main/java/lesson_3/park/Park.java
@@ -1,5 +1,7 @@
 package lesson_3.park;
 
+import lesson_3.Item;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,5 +27,24 @@ public class Park {
 
     public String getParkName() {
         return parkName;
+    }
+
+    public class Attraction extends Item {
+        private String workingHours;
+        private double cost;
+
+        public Attraction(String name, String workingHours, double cost) {
+            super(name);
+            this.workingHours = workingHours;
+            this.cost = cost;
+        }
+
+        @Override
+        public void displayInfo() {
+            System.out.println("Аттракцион: " + name);
+            System.out.println("Время работы: " + workingHours);
+            System.out.println("Стоимость: " + cost);
+            System.out.println("---------------------------");
+        }
     }
 }
